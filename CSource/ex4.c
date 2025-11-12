c9cff9d8fdf111aba42eea3277fbb44875e9c1fa
Rnpc <yankun924@gmail.com>
2025-11-12 07:39:38 +0000

    ex6

---
+++ CSource/ex4.c
@@ -0,0 +1,9 @@
+# include <stdio.h>
+int main(){
+	int age = 16;
+	int height = 10;
+	printf("i'm %d years old.\n");
+	printf("i'm %d inch tall.\n",height);
+	return 0;
+}
+
---
+++ CSource/ex5.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/*comment. */
+int main(int argc, char *argv[]){
+	int distance = 100;
+	//comment 2
+	printf("you are %d miles away.\n",distance);
+	return 0;
+}
